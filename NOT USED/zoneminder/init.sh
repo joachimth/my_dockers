87cf43226991249b56bf87d252136380b4d05961
Joachim Thirsbro <46874512+joachimth@users.noreply.github.com>
2020-06-26 22:23:05 +0200

    init

---
+++ NOT USED/zoneminder/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+docker run -d -t -p 1080:80 \
+    -e TZ='Europe/Copenhagen' \
+    -v ~/zoneminder/events:/var/cache/zoneminder/events \
+    -v ~/zoneminder/images:/var/cache/zoneminder/images \
+    -v ~/zoneminder/mysql:/var/lib/mysql \
+    -v ~/zoneminder/logs:/var/log/zm \
+    --shm-size="512m" \
+    --name zoneminder \
+    zoneminderhq/zoneminder:latest-ubuntu18.04
+    
